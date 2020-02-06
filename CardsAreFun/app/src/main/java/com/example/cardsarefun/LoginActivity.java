669dc2b51ffa68c40cab699d614e2889ff5bb4b9
gabrielzapata19 <zapata5516763@gmail.com>
2020-02-06 12:35:07 -0800

    Added project and login and registration

---
+++ CardsAreFun/app/src/main/java/com/example/cardsarefun/LoginActivity.java
@@ -0,0 +1,107 @@
+package com.example.cardsarefun;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText emailId, passwordId;
+    Button loginBtn;
+    FirebaseAuth firebaseAuth;
+    TextView registerHere;
+
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.emailEditText);
+        passwordId = findViewById(R.id.passwordEditText);
+        loginBtn = findViewById(R.id.registerBtn);
+        registerHere = findViewById(R.id.registerHereTextView);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailId.getText().toString();
+                String password = passwordId.getText().toString();
+
+                //check if email and password fields are empty
+                if(email.isEmpty()){
+                    emailId.setError("Please provide an email, k bye.");
+                    emailId.requestFocus();
+                }
+                else if (password.isEmpty()) {
+                    passwordId.setError("Yo ... enter a password, Ya fool");
+                    passwordId.requestFocus();
+                }
+                else if(email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Both Email and Password fields are empty", Toast.LENGTH_SHORT).show();
+                }
+                //now send fields to firebaseAuth
+                else if (!(email.isEmpty() && password.isEmpty())){
+                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                            }else{
+                                Toast.makeText(LoginActivity.this, "User Login was unsuccessfull", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        registerHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                if(firebaseUser != null){
+                    Toast.makeText(LoginActivity.this, "You are login in! Lets have a toast!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this,HomeActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Please login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+
+    }
+
+
+}
---
+++ CardsAreFun/app/src/main/java/com/example/cardsarefun/RegisterActivity.java
@@ -0,0 +1,86 @@
+package com.example.cardsarefun;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText emailId, passwordId;
+    Button registerBtn;
+    FirebaseAuth myFirebaseAuth;
+    TextView loginHere;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.emailEditText);
+        passwordId = findViewById(R.id.passwordEditText);
+        registerBtn = findViewById(R.id.registerBtn);
+        loginHere = findViewById(R.id.loginHereTextView);
+
+
+        //When user clicks register
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailId.getText().toString();
+                String password = passwordId.getText().toString();
+
+                //check if email and password fields are empty
+                if(email.isEmpty()){
+                    emailId.setError("Please provide an email, k bye.");
+                    emailId.requestFocus();
+                }
+                else if (password.isEmpty()) {
+                    passwordId.setError("Yo ... enter a password, Ya fool");
+                    passwordId.requestFocus();
+                }
+                else if(email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Both Email and Password fields are empty", Toast.LENGTH_SHORT).show();
+                }
+                //now set fields in firebaseAuth object
+                else if (!(email.isEmpty() && password.isEmpty())){
+                    myFirebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                startActivity(new Intent(RegisterActivity.this,HomeActivity.class));
+                            }else{
+                                Toast.makeText(RegisterActivity.this, "User Creation was unsuccessfull", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this, "Oh no! Registeration failed! But its ok.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        loginHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+}
